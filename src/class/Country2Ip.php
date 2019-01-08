ad5e14db8e2ca876095aae71288d588b82a302f0
joffe <joffe.chiu@foxmail.com>
2019-01-08 19:44:06 +0800

    修正

--- src/class/Country2Ip.php
+++ src/class/Country2Ip.php
@@ -17,7 +17,7 @@ class Country2Ip{
  */
     function getIps(string $country_code,int $need_return_ip_number=1){
         if(2 != strlen($country_code)){
-            throw new Exception("Wrong Country Code:".$country_code, 1);
+            throw new \Exception("Wrong Country Code:".$country_code, 1);
         }
         if($this->db === NULL){
             $this->loadDb();
@@ -26,13 +26,13 @@ class Country2Ip{
         $iplist = &$this->db[$country_code];
        // print_r($this->db);
         if(!$iplist){
-            throw new Exception("Wrong Country Code:".$country_code, 1);
+            throw new \Exception("Wrong Country Code:".$country_code, 1);
         }
         $return_data = [];
         for($i=0;$i<$need_return_ip_number;$i++){
-            $k = array_rand($iplist,1);
+            $k = \array_rand($iplist,1);
             $range = &$iplist[$k];
-            $return_data[] = long2ip(rand(inval($range[0]),intval($range[1])));
+            $return_data[] = long2ip(rand(\intval($range[0]),\intval($range[1])));
         }
         return $return_data;
     }
